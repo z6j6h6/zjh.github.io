3e2d19eb3402ebe70d107bc9d02a8655ff479de6
z6j6h6 <2178305070@qq.com>
2025-03-20 15:30:56 +0800

    1

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'awards', 'experience', 'publications'];
+const section_names = ['home', 'table', 'form', 'js'];
 
 
 window.addEventListener('DOMContentLoaded', event => {
